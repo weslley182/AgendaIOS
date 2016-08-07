c88a97562a29fa0ea8b83fb9be95802e9d23f6bf
Weswley Machado <weslley182@gmail.com>
2016-08-07 18:50:42 -0300

    Criação DAO

---
+++ AgendaContatos/ContatoDAO.h
@@ -0,0 +1,13 @@
+//
+//  ContatoDAO.h
+//  AgendaContatos
+//
+//  Created by Mac on 8/7/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContatoDAO : NSObject
+
+@end
---
+++ AgendaContatos/ContatoDAO.m
@@ -0,0 +1,13 @@
+//
+//  ContatoDAO.m
+//  AgendaContatos
+//
+//  Created by Mac on 8/7/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import "ContatoDAO.h"
+
+@implementation ContatoDAO
+
+@end
